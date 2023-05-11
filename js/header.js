4284bd3465d840b4edcdf9b3073787e69d9ad187
atulbhatt-system32 <atulbhatt98@gmail.com>
2023-05-11 15:43:30 +0530

    feat: header

---
+++ js/header.js
@@ -0,0 +1,26 @@
+function addHeader() {
+  const imagePath = `${location.origin}/images/under-header-logo.svg`;
+  const workPath = `${location.origin}/#work-section`;
+  const TEMPLATE = `
+  <div class="flex" >
+        <a class="logo padding-xl" href="/">
+            <img src="${imagePath}" />
+        </a>
+        <div class="panel padding-xl gap-xs" style="color:white;position:fixed; right:0;z-index:999;  mix-blend-mode: difference;">
+            <a class="header-link" href=${workPath}><h4>Work</h4></a>
+            <a class="header-link" href="https://store.weareunder.design"><h4>Store</h4></a>
+            <a class="header-link" href="mailto:hello@weareunder.design"><h4>Contact</h4></a>
+        </div>
+    </div>
+  `;
+
+  class UnderHeader extends HTMLElement {
+    constructor() {
+      super();
+      this.innerHTML = TEMPLATE;
+    }
+  }
+  customElements.define("under-header", UnderHeader);
+}
+
+addHeader();
